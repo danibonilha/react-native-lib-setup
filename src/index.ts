af73a2a8a98183d50719e3b04000dca439492589
danibonilha <dani.bonilha@live.com>
2020-09-17 23:09:23 -0300

    feat(Main): Add rn project verification

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
+import { RelativePlatormPath } from "./helpers/enums";
+import { directoryExists } from "./helpers/files";
+import { logger } from "./helpers/logger";
+import { strings } from "./helpers/strings";
+
+
+const checkRNProject = (): void => {
+  Object.keys(RelativePlatormPath).map((path) => {
+    if (!directoryExists(path)) {
+      logger.error(strings.nonRNProjectError(path))
+      throw Error()
+    }
+  });
+}
+
+
 export const run = (): void => {
   console.log('run')
+
+  logger.banner()
+  checkRNProject()
 }
 
 run()
